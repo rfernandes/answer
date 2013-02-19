cdbba893b094a2d9077f86bec8acd441093676cf
psitech <cyber_fusion@gmx.net>
2013-02-19 20:39:55 +0000

    * Still learning this Git thing...

--- src/archive/ws_xml_oarchive.cpp
+++ src/archive/ws_xml_oarchive.cpp
@@ -8,11 +8,10 @@
 
 namespace boost {
 namespace archive {
-using namespace answer::archive;
 
-template class detail::archive_serializer_map<ws_xml_oarchive>;
-template class basic_xml_oarchive<ws_xml_oarchive> ;
-template class xml_oarchive_impl<ws_xml_oarchive> ;
+template class detail::archive_serializer_map<answer::archive::ws_xml_oarchive>;
+template class basic_xml_oarchive<answer::archive::ws_xml_oarchive> ;
+template class xml_oarchive_impl<answer::archive::ws_xml_oarchive> ;
 
 }
 }
